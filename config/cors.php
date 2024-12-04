c94dcbc179ec9594bdb4a17a3ff7ed5855059185
Omar El Taha Santos <omareltahasan6@gmail.com>
2024-12-04 16:40:35 +0100

    allow cors

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-    'https://fichajes-damia-front-5x5w6b09a-omareltahasantos-projects.vercel.app/',
+    'https://fichajes-damia-front-5x5w6b09a-omareltahasantos-projects.vercel.app',
     'https://sistemasmedioambientalesfichajes.netlify.app',
     'http://localhost:3000', // Si estás trabajando en local
 ],
